bb30e9ff6b90561bdf053078a825787c0ab3947b
Amit Paswan <ak9425391@gmail.com>
2023-12-29 18:05:37 +0530

    message addded

--- react-router-dom/src/components/common/Messages.jsx
+++ react-router-dom/src/components/common/Messages.jsx
@@ -1,25 +1,26 @@
 const Messages = () => {
   const Messages = [
-    { id: 1, text: "Hello, how are you?" },
-    { id: 2, text: "Good Morning." },
-    { id: 3, text: "This is a sample message." },
-    { id: 4, text: "Have a great day!" },
-    { id: 5, text: "Coding is fun" },
-    { id: 6, text: "Just testing messages." },
-    { id: 7, text: "Learning JavaScript." },
-    { id: 8, text: "Learning React." },
-    { id: 9, text: "Almost done!" },
-    { id: 10, text: "Good Night." },
+    { id: 1, title: "greeting", text: "Hello, how are you?" },
+    { id: 2, title: "morning", text: "Good Morning." },
+    { id: 3, title: "sample", text: "This is a sample message." },
+    { id: 4, title: "great", text: "Have a great day!" },
+    { id: 5, title: "coding", text: "Coding is fun" },
+    { id: 6, title: "testing", text: "Just testing messages." },
+    { id: 7, title: "learning", text: "Learning JavaScript." },
+    { id: 8, title: "react", text: "Learning React." },
+    { id: 9, title: "almost", text: "Almost done!" },
+    { id: 10, title: "night", text: "Good Night." },
   ];
 
   return (
     <div className="bg-gray-800 text-white h-screen flex flex-col justify-center items-center w-screen font-semibold">
       {Messages.map((message, index) => (
         <>
-          <div key={index} className="flex">
+          <div key={index} className="flex mt-2">
             <div> {message.id}.</div>
             <br />
-            <div> {message.text}</div>
+
+            <div>{message.text}</div>
           </div>
         </>
       ))}
