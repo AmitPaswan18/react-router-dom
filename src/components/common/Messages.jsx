233becb13710009eb2c6e502f966db42cf74fdec
Amit Paswan <ak9425391@gmail.com>
2024-01-02 18:05:51 +0530

    made utils for messesges

--- src/components/common/Messages.jsx
+++ src/components/common/Messages.jsx
@@ -1,50 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { messages } from "../../utils";
 
 const Message = () => {
-  const messages = [
-    {
-      id: 1,
-      title: "Greeting",
-      text: "Hello, how are you? It's wonderful to see you today. I hope your day is filled with joy and positivity.",
-    },
-    {
-      id: 2,
-      title: "Morning",
-      text: "Good Morning. Rise and shine! Embrace the new day with enthusiasm and a heart full of gratitude.",
-    },
-    {
-      id: 3,
-      title: "Sample",
-      text: "This is a sample message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida tristique justo, ac aliquam quam dapibus vel.",
-    },
-    {
-      id: 4,
-      title: "Great",
-      text: "Have a great day! May your day be filled with accomplishments, laughter, and moments that make your heart happy.",
-    },
-    {
-      id: 5,
-      title: "Coding",
-      text: "Coding is fun and rewarding. Dive into the world of programming, explore new technologies, and bring your ideas to life.",
-    },
-    {
-      id: 6,
-      title: "Testing",
-      text: "Just testing messages. This is a placeholder text to check the formatting and appearance of the messages in the application.",
-    },
-    {
-      id: 7,
-      title: "Learning",
-      text: "Learning JavaScript opens up a world of possibilities. Master the language and unlock the potential to build amazing web applications.",
-    },
-    {
-      id: 8,
-      title: "React",
-      text: "Learning React is an exciting journey. Build interactive user interfaces and create dynamic web experiences with the power of React.",
-    },
-  ];
-
   const [active, setActive] = useState(true);
 
   const navigate = useNavigate();
